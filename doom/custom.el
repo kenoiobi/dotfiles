7dd742261db5bee231cebc06d3ce79ba1445bc86
kenoiobi <kenoiobi@gmail.com>
2025-01-25 20:51:43 -0300

    Create custom.el

---
+++ doom/custom.el
@@ -0,0 +1,10 @@
+(custom-set-variables
+ '(compile-command "./build.sh")
+ '(dired-dwim-target t)
+ '(display-line-numbers-type 'relative)
+ '(display-time-24hr-format t)
+ '(display-time-mode t)
+ '(tramp-terminal-type "tramp")
+ '(tramp-verbose 6))
+(custom-set-faces
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#282c34" :foreground "#bbc2cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 180 :width normal :foundry "PfEd" :family "DejaVuSansM Nerd Font")))))
