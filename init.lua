be4e5c2ee2f313c54bacbcfc2f3cb129882fb9d9
kenoiobi <kenoiobi@gmail.com>
2025-02-07 10:50:49 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -38,7 +38,6 @@ require("lazy").setup({
 	'nvim-telescope/telescope.nvim',
 	'ahmedkhalf/project.nvim',
 	'natecraddock/workspaces.nvim',
-	"ThePrimeagen/harpoon",
   },
   -- install = { colorscheme = { "habamax" } },
   checker = { enabled = true },
@@ -163,29 +162,3 @@ require("Comment").setup()
 require('telescope').load_extension('projects')
 require("project_nvim").setup()
 require("workspaces").setup()
-
-local harpoon = require("harpoon")
-
-harpoon:setup({
-	settings = {
-		save_on_toggle = true,
-		sync_on_ui_close = true
-	}
-})
-vim.keymap.set("n", "<leader>s", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
-vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
-vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
-vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
-vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
-vim.keymap.set("n", "<leader>0", function() harpoon:list():select(10) end)
-
-
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
