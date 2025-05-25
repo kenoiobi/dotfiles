c419ae5f544dc78ab3d4a90abf3fa39389b406c0
Kayon Jaime <kenoiobi@gmail.com>
2025-05-25 15:15:50 -0300

    changing workflow from copy to symlinks

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo pacman -S - < pacman.txt
+# sudo pacman -S - < pacman.txt
 cp -r ./.config/ ~/
 sudo cp 00-keyboard.conf /etc/X11/xorg.conf.d/
 
@@ -11,3 +11,16 @@ cd ~/git
 git clone https://github.com/kenoiobi/st
 cd dwm
 sudo make clean install
+
+cd ~/
+
+ln -s ~/git/dotfiles/home/.emacs
+ln -s ~/git/dotfiles/home/.bookmark
+ln -s ~/git/dotfiles/home/.vimrc
+ln -s ~/git/dotfiles/home/.xinitrc
+ln -s ~/git/dotfiles/home/.Xresources
+ln -s ~/git/dotfiles/home/.zshrc
+
+mkdir .emacs.d
+cd ~/.emacs.d/
+ln -s ~/git/dotfiles/home/.emacs.d/snippets/
