7522acb6672a72a3752320a35670455d6b415617
kayon <kayon@localhost.localdomain>
2025-07-24 11:28:14 -0300

    finally fixing (i hope)

--- zsh/.zsh.d/plugins.zsh
+++ zsh/.zsh.d/plugins.zsh
@@ -1,6 +1,6 @@
 # Antidote for managing files
 source ~/.zsh.d/antidote/antidote.zsh
-antidote load ~/dotfiles/zsh/.zsh_plugins.txt
+antidote load ~/.zsh.d/.zsh_plugins.txt
 
 bindkey "^R" history-incremental-search-backward # backup history when o fzf in system
 source <(fzf --zsh)
