93d6812e2516eac26a1fbf365e16525a47d487b8
kayon <kayon@debian>
2025-09-15 11:58:02 -0300

    adding stuff

--- zsh/.zsh.d/.zsh_plugins.zsh
+++
@@ -1,2 +0,0 @@
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
