6790c58925f692282875ee010850ba372303ff70
kenoiobi <kenoiobi@gmail.com>
2026-03-08 21:35:14 -0300

    forgor to revert thing

--- zsh/.zsh.d/keys.zsh
+++ zsh/.zsh.d/keys.zsh
@@ -1,6 +1,6 @@
 fzf-history-widget() {
   local output key selected query
-  output=$(fc -rln 1 | awk '!seen[$0]++' | fzf --print-query --expect=alt-enter --height=40% --reverse --tac +s -q "$LBUFFER")
+  output=$(fc -rln 1 | awk '!seen[$0]++' | fzf --print-query --expect=alt-enter --height=40% --reverse +s -q "$LBUFFER")
   query=$(echo "$output" | sed -n '1p')
   key=$(echo "$output" | sed -n '2p')
   selected=$(echo "$output" | sed -n '3p')
