b4ce0b2c1278598d3e20e9b3ee4283ac067c9a23
kenoiobi <kenoiobi@gmail.com>
2026-04-07 23:42:55 -0300

    adding cursor

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-stow bash xorg lazyvim tmux lf rofi yazi alacritty redshift doom hyprland waybar
+stow bash xorg lazyvim tmux lf rofi yazi alacritty redshift doom hyprland waybar cursor
 
 cp ./Consolas-Nerd-Font/*.ttf /usr/local/share/fonts/
